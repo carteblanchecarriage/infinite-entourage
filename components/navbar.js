96a24e15f194a39087f57569416bb17e5debd85d
klondike <klondike.builder@protonmail.com>
2026-02-20 15:12:56 -0500

    Remove Supabase auth pages and deprecated files
    - Deleted login.js - no longer using Supabase Auth (using fingerprint instead)
    - Deleted supabaseClient.js - breaking build with wrong env var names
    - Deleted Account.js - deprecated auth helpers component
    - Removed login link from navbar
    - App now uses anonymous fingerprint-based credits (no sign-in required)

--- components/navbar.js
+++ components/navbar.js
@@ -4,9 +4,6 @@ export default function Navbar() {
     <>
       <div className='mx-auto max-w-4xl flex justify-between items-center text-2xl font-semibold italic m-6'>
         <Link href='/'>INFINITE ENTOURAGE</Link>
-        <Link href='/login' className='text-right text-sm italic'>
-          LOGIN
-        </Link>
       </div>
     </>
   );
